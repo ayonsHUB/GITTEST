0dd5c79e6486e565c695274f1791799df0122649
Arif Faysal <ayon.arif.10@gmail.com>
2022-01-20 14:29:49 -0500

    first-commit

---
+++ src/gittest/GITTEST.java
@@ -0,0 +1,19 @@
+package gittest;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Arif Faysal
+ */
+public class GITTEST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hi");
+        
+    }
+
+}
