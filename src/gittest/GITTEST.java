15415e8a5469041526c2e7fb4f33789988020ba2
Arif Faysal <ariffaysal101@gmail.com>
2022-01-27 10:31:23 -0500

    test-method-created

--- src/gittest/GITTEST.java
+++ src/gittest/GITTEST.java
@@ -6,6 +6,15 @@ package gittest;
  * @author Arif Faysal
  */
 public class GITTEST {
+    
+    
+    public static boolean isTrue( boolean var ) {
+        if( var == true ){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -15,6 +24,8 @@ public class GITTEST {
         System.out.println("Hi");
         System.out.println("Arif Here");
         System.out.println("Modified directly from Github !");
+        
+        System.out.println( isTrue(true) );
         
     }
 
